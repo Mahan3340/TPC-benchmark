984ea01e08904a7effb320ce7a2c46ee2f02d3af
Mahan <mahan_tabatabai@yahoo.com>
2019-01-16 20:48:21 +0330

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -19,10 +19,4 @@ group by
 	
 order by
 	o_orderpriority
-LIMIT 1;
 COMMIT;
-
-
-o_temp = orders.filter("o_orderdate >= '1995-01-01' ").filter("o_orderdate < '1995-04-01'")
-l_temp = lineitem.filter('l_commitdate < l_receiptdate')
-l_o = l_temp.join(o_temp, l_temp.L_ORDERKEY == o_temp.O_ORDERKEY).groupBy('o_orderpriority').agg(F.count('*').alias("order_count")).select(o_temp.O_ORDERPRIORITY,'order_count').orderBy('o_orderpriority')
