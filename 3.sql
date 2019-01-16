c654bd06b1f0f2d1376560f6f87ee79cb6feb031
Mahan <mahan_tabatabai@yahoo.com>
2019-01-16 20:48:01 +0330

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -22,17 +22,3 @@ order by
 	o_orderdate
 LIMIT 10;
 COMMIT;
-
-c_temp = customer.filter("c_mktsegment = 'AUTOMOBILE'")
-l_temp = lineitem.filter("l_shipdate > '1995-03-13' ")
-o_temp = orders.filter("o_orderdate < '1995-03-13' ")
-l_o = l_temp.join(o_temp,l_temp.L_ORDERKEY == o_temp.O_ORDERKEY)
-c_o = c_temp.join(l_o,c_temp.C_CUSTKEY == l_o.O_CUSTKEY)
-
-res = c_o.select(c_o.O_ORDERKEY,c_o.O_ORDERDATE,c_o.O_SHIPPRIORITY).join(l_temp,c_o.O_ORDERKEY == l_temp.L_ORDERKEY).select(c_o.O_ORDERKEY,c_o.O_ORDERDATE,c_o.O_SHIPPRIORITY,l_temp.L_ORDERKEY,c_o.O_ORDERKEY,c_o.O_ORDERDATE,l_temp.L_EXTENDEDPRICE,l_temp.L_DISCOUNT)
-res2 = res.groupBy('l_orderkey', 'o_orderdate', 'o_shippriority').agg(F.sum(res.L_EXTENDEDPRICE *(1 -l_temp.L_DISCOUNT)).alias("revenue")).sort(res.O_ORDERDATE,F.desc("revenue")) 
-
-
-
-
-
