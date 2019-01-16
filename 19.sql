f75ae2586d3ab967256dc0dc9030886fdea56b98
Mahan <mahan_tabatabai@yahoo.com>
2019-01-16 20:46:54 +0330

    Update 19.sql

--- 19.sql
+++ 19.sql
@@ -34,29 +34,4 @@ where
 		and l_shipmode in ('AIR', 'AIR REG')
 		and l_shipinstruct = 'DELIVER IN PERSON'
 	)
-LIMIT 1;
 COMMIT;
-
-
-
-datetime.datetime.now().time()
-containers1 = ["SM CASE","SM BOX","SM PACK","SM PKG"]
-containers2 = ["MED BAG","MED BOX","MED PKG","MED PACK"]
-containers3 = ["LG CASE","LG BOX","LG PACK","LG PKG"]
-p_l = part.join(lineitem,part.P_PARTKEY == lineitem.L_PARTKEY).filter(("l_shipmode like '%AIR%' " or "l_shipmode like '%AIR REG%' ") and "l_shipinstruct like '%DELIVER IN PERSON%'")
-p_l_2 = p_l.filter( ( (p_l.P_BRAND.like("%Brand#54%")) & (p_l.P_CONTAINER.isin(containers1)) & (p_l.L_QUANTITY >= 8) & (p_l.L_QUANTITY <= 18) &  (p_l.P_SIZE >= 1) & (p_l.P_SIZE <= 10) ) | ((p_l.P_BRAND.like("%Brand#22%")) & (p_l.P_CONTAINER.isin(containers2)) & (p_l.L_QUANTITY >= 3) & (p_l.L_QUANTITY <= 23) &  (p_l.P_SIZE >= 1) & (p_l.P_SIZE <= 5) ) | ((p_l.P_BRAND.like("%Brand#51%")) & (p_l.P_CONTAINER.isin(containers3)) & (p_l.L_QUANTITY >= 22) & (p_l.L_QUANTITY <= 32) &  (p_l.P_SIZE >= 1) & (p_l.P_SIZE <= 15) ) ).select((p_l.L_EXTENDEDPRICE*(1-p_l.L_DISCOUNT)).alias("VOLUME")).agg(F.sum("VOLUME"))
-p_l_2.show()
-datetime.datetime.now().time()
-
-
-
-
-
-
-
-
-
-
-
-
-
