479d5b95ca19850febb6538c01a7bf123c58b17a
Mahan <mahan_tabatabai@yahoo.com>
2019-01-16 20:47:10 +0330

    Update 20.sql

--- 20.sql
+++ 20.sql
@@ -44,20 +44,3 @@ order by
 	s_name
 LIMIT 1;
 COMMIT;
-
-
-datetime.datetime.now().time()
-joinCondition = [l_temp.L_SUPPKEY == p_ps.PS_SUPPKEY, l_temp.L_PARTKEY == p_ps.PS_PARTKEY]
-l_temp = lineitem.filter("l_shipdate >= '1993-01-01' " and "l_shipdate < '1994-01-01' ").groupBy('L_PARTKEY','L_SUPPKEY').agg(F.sum(lineitem.L_QUANTITY*0.5).alias("SUM_QUANTITY"))
-n_temp = nation.filter("n_name like '%CANADA%' ")
-n_s = supplier.select('S_SUPPKEY','S_NAME','S_NATIONKEY','S_ADDRESS').join(n_temp,supplier.S_NATIONKEY == n_temp.N_NATIONKEY)
-p_temp = part.filter(part.P_NAME.startswith("forest")).select('P_PARTKEY')
-p_ps = p_temp.join(partsupp,part.P_PARTKEY == partsupp.PS_PARTKEY)
-p_ps_l = p_ps.join(l_temp,joinCondition).filter(p_ps.PS_AVAILQTY > l_temp.SUM_QUANTITY).select(p_ps.PS_SUPPKEY)
-p_ps_l_n_s = p_ps_l.join(n_s,n_s.S_SUPPKEY == p_ps_l.PS_SUPPKEY).select(n_s.S_NAME,n_s.S_ADDRESS).sort(n_s.S_NAME)
-datetime.datetime.now().time()
-p_ps_l_n_s.show()
-datetime.datetime.now().time()
-
-
-
