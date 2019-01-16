921c32daf4aa1036c790abc78d126cecd98f1c97
Mahan <mahan_tabatabai@yahoo.com>
2019-01-16 20:42:29 +0330

    Update 11.sql

--- 11.sql
+++ 11.sql
@@ -26,41 +26,4 @@ group by
 		)
 order by
 	value desc
-LIMIT 1;
 COMMIT;
-
-
-
-val mul = udf { (x: Double, y: Int) => x * y }
-    val mul01 = udf { (x: Double) => x * 0.0001 }
-
-    val tmp = nation.filter($"n_name" === "GERMANY")
-      .join(supplier, $"n_nationkey" === supplier("s_nationkey"))
-      .select($"s_suppkey")
-      .join(partsupp, $"s_suppkey" === partsupp("ps_suppkey"))
-      .select($"ps_partkey", mul($"ps_supplycost", $"ps_availqty").as("value"))
-    // .cache()
-
-    val sumRes = tmp.agg(sum("value").as("total_value"))
-
-    tmp.groupBy($"ps_partkey").agg(sum("value").as("part_value"))
-      .join(sumRes, $"part_value" > mul01($"total_value"))
-      .sort($"part_value".desc)
-
-
-n_temp = nation.filter("n_name like '%ARGENTINA%' ")
-n_s = n_temp.join(supplier,nation.N_NATIONKEY == supplier.S_NATIONKEY).select('S_SUPPKEY')
-n_s_ps = n_s.join(partsupp,n_s.S_SUPPKEY == partsupp.PS_SUPPKEY).select('PS_PARTKEY',(partsupp.PS_SUPPLYCOST*partsupp.PS_AVAILQTY).alias("VALUE"))
-sum = n_s_ps.agg(F.sum("VALUE").alias("TOTAL_VALUE"))
-res1 = n_s_ps.groupBy('PS_PARTKEY').agg(F.sum("VALUE").alias("PART_VALUE"))
-res2 = res1.crossJoin(sum).filter(res1.PART_VALUE > (sum.TOTAL_VALUE*0.0001)).sort(F.desc("PART_VALUE"))
-
-datetime.datetime.now().time()
-res2.show()
-datetime.datetime.now().time()
-
-
-
-
-
-
