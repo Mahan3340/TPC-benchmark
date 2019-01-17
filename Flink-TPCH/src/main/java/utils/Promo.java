bfe4bfed44520fc32f69273efb665cf357fa7104
Mahan <mahan_tabatabai@yahoo.com>
2019-01-17 11:22:39 +0330

    Update Promo.java

--- Flink-TPCH/src/main/java/utils/Promo.java
+++ Flink-TPCH/src/main/java/utils/Promo.java
@@ -5,7 +5,6 @@ public class Promo extends org.apache.flink.table.functions.ScalarFunction {
     public float eval(String x, Float y) {
         if (x.startsWith("PROMO")) {
             return y;
-
         }
         return 0;
     }
